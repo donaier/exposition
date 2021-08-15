a2f194756a28a2008562d7c801f8fe2a3e2e1143
donat baier <donat.baier@gmail.com>
2021-08-15 17:37:12 +0200

    exhibit list

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,17 @@
 class HomeController < ApplicationController
   def index
+    # @exhibits = Exhibit.all
+    @exhibits = [
+      {
+        poster: 'chlausepragel.jpg',
+        title: 'chlause/pragel',
+        date: '13. Aug 2021',
+        description: '
+          100+ km tour über 2 pässe <br>
+          cowboycamping am ufer, vermutlich wars eine tropennacht <br><br>
+          is this #bikepacking?'
+      }
+    ]
+
   end
 end
