431b8632b1482ccd8dbc45636910516470403b65
donat baier <donat.baier@gmail.com>
2021-08-15 14:43:35 +0200

    fork/copy

---
+++ app/models/user.rb
@@ -0,0 +1,5 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable, :omniauthable, :registerable, :recoverable, :rememberable,
+  devise :database_authenticatable, :validatable
+end
